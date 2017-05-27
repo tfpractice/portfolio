c9af0e131bfbd82a67ab5743a4839767d034e886
theory friction practice <jackson.brebnor@gmail.com>
2017-05-26 20:52:16 -0400

    adds route files

---
+++ src/components/main/about.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Text from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import Card, { CardActions, CardContent, CardHeader, } from 'material-ui/Card';
+
+const styles = { paddingTop: '5rem', };
+
+const stateToProps = state => (state);
+
+const About = () => (
+  <Grid container justify="center" style={styles} >
+    <Grid item sm={6}>
+      <Card raised>
+        <CardHeader title="Welcome to My site" />
+        <CardContent>
+          <Text type="subheading">
+            A web developer with a passion for functional programming and application architecture
+             </Text>
+        </CardContent>
+        <CardActions>
+          <Button compact>Learn More</Button>
+        </CardActions>
+      </Card>
+    </Grid>
+  </Grid>
+    );
+
+export default (About);
