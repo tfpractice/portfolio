228cbebf3726bc04f4ccb8eb056335584186f18b
theory friction practice <jackson.brebnor@gmail.com>
2017-05-27 00:08:57 -0400

    single project view and routes

--- src/components/projects/main.js
+++ src/components/projects/main.js
@@ -1,10 +1,11 @@
 import React, { Component, } from 'react';
 import { connect, } from 'react-redux';
-import { Route, Switch, } from 'react-router-dom';
+import { Link, Route, Switch, } from 'react-router-dom';
 import Grid from 'material-ui/Grid';
 import Paper from 'material-ui/Paper';
 
 import { containers, } from '../../store/projects';
+import Single from './single';
 const { WithAll, } = containers;
 
 // /const
@@ -16,9 +17,13 @@ const Projects = (props) => {
   return (
     <Grid container justify="center" align="center" direction="column">
       <Grid item sm={12}>
+        {props.projects.map(p =>
+          (<Link to={`${props.match.url}/${p.id}`} key={p.id}>
+            { p.title}
+          </Link>))}
         <h1>projects</h1>
         <Switch >
-          {/* <Route path={`${props.match.url}/:project_id`}  component={Game} /> */}
+          <Route path={`${props.match.url}/:project_id`} component={Single} />
           {/* <Route path="/" exact component={About} /> */}
         </Switch>
       </Grid>
--- src/store/projects/fragments.js
+++ src/store/projects/fragments.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 export const PROJECT_INFO = gql`
   fragment projectInfo on Project{
     id
+    title
     description
   }
 `;
