7502e53975cbea4bae8295221edf2379b01cc621
theory friction practice <jackson.brebnor@gmail.com>
2017-06-08 21:44:21 -0400

    doispaly cleanup

--- src/components/main/endo.js
+++ src/components/main/endo.js
@@ -13,7 +13,6 @@ const mygons = range(20).map(i => setNumSides(6)(polygon(i, i, 10)));
 const style = {
   width: '100%',
   height: '100%',
-
 };
 const xBox = data => box => d3.scaleLinear()
   .domain(d3.extent(data, g => g.x))
@@ -26,97 +25,73 @@ const yBox = data => box => d3.scaleLinear()
 const rBox = data => box => d3.scaleLinear()
   .domain(d3.extent(data, g => g.radius))
   .range([ box.bottom / data.length, box.top / data.length, ]);
-  
+
+const getBox = sel => sel.node().getBoundingClientRect();
+
 class Endo extends Component {
   componentDidMount() {
-    const vSelect = d3.select('.endovis');
-    const getBox = sel => sel.node().getBoundingClientRect();
-    const endoBox = getBox(vSelect);
-    const vBox = vSelect.node().getBoundingClientRect();
-
-    const xScale = d3.scaleLinear()
-    .domain(d3.extent(mygons, g => g.x))
-    .range([ vBox.width * 0.1, vBox.width * 0.9, ]);
-  
-    const yScale = d3.scaleLinear()
-    .domain(d3.extent(mygons, g => g.y))
-    .range([ vBox.height * 0.1, vBox.height * 0.9, ]);
+    const endoBox = getBox(d3.select('.endovis'));
     
-    const rScale = d3.scaleLinear()
-    .domain(d3.extent(mygons, g => g.radius))
-    .range([ vBox.bottom / mygons.length, vBox.top / mygons.length, ]);
-    
-    const visBox = d3
-    .select('.visbox')
-    .select('.endovis')
-    .selectAll('.polygon')
-    .data(mygons, (d, i) => {
-      const data = d;
-
-      return i;
-    })
-    .insert('circle')
-    .attr('cx', d => xBox(mygons)(endoBox)(d.x))
-    .attr('cy', d => yBox(mygons)(endoBox)(d.y))
-    .attr('r', d => rBox(mygons)(endoBox)(d.radius))
-    .attr('stroke', '#f0f')
-    .attr('fill', 'none');
+    d3
+      .select('.visBox')
+      .select('.endovis')
+      .selectAll('.polygon')
+      .data(mygons)
+      .append('circle')
+      .attr('cx', d => xBox(mygons)(endoBox)(d.x))
+      .attr('cy', d => yBox(mygons)(endoBox)(d.y))
+      .attr('r', d => rBox(mygons)(endoBox)(d.radius))
+      .attr('stroke', '#f0f')
+      .attr('fill', 'none');
     
     const polyLine = (p, idx) => {
-      const wCent = tickPoints(p)(3).reduce((p, n, i) => {
-        console.log('p,n,i', p, n, i);
-        
-        return (i && (i % 3 === 0)) ? p.concat(center(p), n) : p.concat(n);
-      }, []);
-      const localD = vertices(p).concat(wCent);
-
+      const localBox = d3.select(`#vis${idx}`);
+      const localD = tickPoints(p)(3).reduce((p, n, i) =>
+        (i && (i % 3 === 0)) ? p.concat(center(p), n) : p.concat(n), vertices(p));
+      
       return d3.line()
-        .x(d => xBox([{ x: 0, y: 0, }, p, ])(getBox(d3.select(`#vis${idx}`)))(d.x))
-        .y(d => xBox([ p, ])(getBox(d3.select(`#vis${idx}`)))(d.y))(localD);
+        .x(d => xBox(localD)(getBox(localBox))(d.x))
+        .y(d => xBox(localD)(getBox(localBox))(d.y))(localD);
     };
-
+    
     d3
-
-      // .select('.disBox')
-      // .select('.disVis')
-      .selectAll('.disVis')
+      .selectAll('.disBox')
       .selectAll('.displayed')
       .data(mygons)
-
-      // .append('path')
       .attr('d', polyLine)
       .attr('stroke', '#0f0')
+      .attr('stroke-width', '0.5')
       .attr('fill', 'none');
   }
+  
   render() {
     return (
       <Grid container justify="center">
-        <Grid item xs={11} className="visbox">
+        <Grid item xs={11} className="visBox">
           <svg className="endovis" style={style}>
             {mygons.map((p, i) => (
               <g className="polygon" key={i} />)
             )}
-            {mygons.map((p, i) => (
-              <g className="displayed" key={i} />)
-            )}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      </svg>
+          </svg>
         </Grid>
-        {mygons.map((p, i) => (
-          <Grid item xs={2} className="disBox">
-            <svg className="disVis" id={`vis${i}`} style={style}>
-              {/* {mygons.map((p, i) => ( */}
-              <path className="displayed" key={i} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                </svg>
+        <Grid item xs={11}>
+          <Grid container justify="center" align="flex-start" className="tickBox">
+            {mygons.map((p, i) => (
+              <Grid item xs={1} className="disBox" id={`vis${i}`} key={i}>
+                <svg className="disVis">
+                  <path className="displayed" />
+                </svg>
+              </Grid>
+            ))}
           </Grid>
-        )
-        )}
+        </Grid>
         <Grid item xs={11}>
           <Card raised>
             <CardHeader title="Welcome to My site" />
             <CardContent>
               <Text secondary type="body1">
                 {'FUCKINPOLYGONS'}
-
               </Text>
-
             </CardContent>
             <CardActions>
               <Button compact>Learn More</Button>
