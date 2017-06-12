442c3132425ab1f0b972e53128bc54867927d1d6
theory friction practice <jackson.brebnor@gmail.com>
2017-06-12 18:52:51 -0400

    Data folder

---
+++ src/components/projects/single/data/fenugreek.js
@@ -0,0 +1,41 @@
+
+export const info = 'a library of utilities for functionally handling collections in javascript';
+
+export const highlights = [
+  'allows a high degree of interoperability between arrays, Maps, and Sets collections such as Maps',
+  'recasts native variadic functions as partially applied/curried unary functions   of composability',
+  'implements mathematical set functions',
+];
+
+export const technicals = [ 'built with es6, and bundled with Rollup', ];
+export const future = [
+  `make this part of a monorepo in the way of MaterialUI, enabling a more fluent,
+  and specific import export process`,
+];
+
+const files = [
+  {
+    data: 'allows a high degree of interoperability between arrays, Maps, and Sets collections such as Maps',
+    blobUrl: '',
+  },
+  { data: 'recasts native variadic functions as partially applied/curried unary functions   of composability', },
+  { data: 'implements mathematical set functions', },
+];
+
+export const slides = files;
+export const devEx = [
+  `this was my first attempt at writing a library from a functional
+   perspective. And most of the complicated issues were architectural. On a modular
+   level, exporting a single function per module seemed like a draconian exercise in 
+   code-etiqutte, so I decided to group similar functions together. This led to
+    complications with circular dependencies and  naming conventions.`,
+    
+  `On a per fucntion level, the decision to partially apply presented a number of orthogonal issues.
+    If the native function such as Array#push(elem) requires both an array and an element,
+    should it be written as push(array)(elem) or push(elem)(array). Thinking that this 
+    module would be collection focused, I decided that most of the fucntions should take
+    in an array, and would be simplify api learning curve. 
+    This question is still unresolved, and some functions, partiuarly those like
+    Array#map, Array#filter, and Array#reduce have permuted counterParts`,
+];
+export const thoughts = devEx;
---
+++ src/components/projects/single/data/index.js
@@ -0,0 +1,21 @@
+import * as Fenugreek from './fenugreek';
+import * as Graph from './graph';
+const xData = {
+  technicals: [],
+  techniques: [],
+  slides: [],
+  highlights: [],
+  thoughts: [],
+  devEx: [],
+  future: [],
+};
+
+export const slugMap = {
+  'fenugreek-collections': Fenugreek,
+  'graph-curry': Graph,
+};
+
+export const slugData = (slug) => {
+  console.log('Object.keys(slugMap)', Object.keys(slugMap));
+  return new Set(Object.keys(slugMap)).has(slug) ? slugMap[slug] : xData;
+};
