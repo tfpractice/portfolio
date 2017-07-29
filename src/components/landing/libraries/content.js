193cfc0ccffcdbf20632de6383a55223389d2946
theory friction practice <jackson.brebnor@gmail.com>
2017-07-28 22:11:52 -0400

    progress

--- src/components/landing/libraries/content.js
+++ src/components/landing/libraries/content.js
@@ -4,13 +4,13 @@ When designing business logic I focus on writing small code is flexible and exte
 This quality is exemplified in my work with graph algorithms which has been the basis for things ranging from connect four to gin rummy.`;
 
 const details = [
-  { caption: 'leverages firebase and redux for integrated state management', },
-  { caption: 'encapsulates separate game state between clients', },
-  { caption: 'foolproof updates with zero intermediate state', },
+  { caption: 'leverages firebase and redux for integrated state management' },
+  { caption: 'encapsulates separate game state between clients' },
+  { caption: 'foolproof updates with zero intermediate state' },
 ];
 
 const C4details = [
-  { caption: 'integrates redux and firebase authentication with redux store', },
-  { caption: 'integrates react rendering with d3 visualization', },
-  { caption: 'foolproof updates with zero intermediate state', },
+  { caption: 'integrates redux and firebase authentication with redux store' },
+  { caption: 'integrates react rendering with d3 visualization' },
+  { caption: 'foolproof updates with zero intermediate state' },
 ];
--- src/components/landing/libraries/view.js
+++ src/components/landing/libraries/view.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
 import Text from 'material-ui/Typography';
-import Card, { CardContent, CardHeader, } from 'material-ui/Card';
-import { connect, } from 'react-redux';
-import { MarkdownPreview, } from 'react-marked-markdown';
+import Card, { CardContent, CardHeader } from 'material-ui/Card';
+import { connect } from 'react-redux';
+import { MarkdownPreview } from 'react-marked-markdown';
 
-import { HexCard, } from '../../misc';
-import { libFilt, } from '../../../utils';
+import { HexCard } from '../../misc';
+import { libFilt } from '../../../utils';
 import LandingList from '../../projects/landingList';
-import { content, } from './content';
+import { content } from './content';
 
-const stateToProps = ({ projects, }) => ({ libs: libFilt(projects), });
+const stateToProps = ({ projects }) => ({ libs: libFilt(projects) });
 
-const Libs = ({ libs, }) => (
-  <Grid container justify="center" className="libraries-info">
-    <Grid item xs={11} >
+const Libs = ({ libs }) =>
+  (<Grid container justify="center" className="libraries-info">
+    <Grid item xs={11}>
       <HexCard raised>
-        <CardHeader title="Testable and Flexible"/>
+        <CardHeader title="Testable and Flexible" />
         <CardContent>
           <Text component="div" color="secondary" type="body2">
-            <MarkdownPreview value={content}/>
+            <MarkdownPreview value={content} />
           </Text>
         </CardContent>
       </HexCard>
@@ -27,7 +27,6 @@ const Libs = ({ libs, }) => (
     <Grid item xs={11}>
       <LandingList items={libs} />
     </Grid>
-  </Grid>
-);
+  </Grid>);
 
 export default connect(stateToProps)(Libs);
