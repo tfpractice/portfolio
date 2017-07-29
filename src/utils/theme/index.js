6089705f1ec95e7563fb14f34d7a51e5ad97cba8
theory friction practice <jackson.brebnor@gmail.com>
2017-07-28 21:24:08 -0400

    adds prettier dependencies

--- src/utils/theme/index.js
+++ src/utils/theme/index.js
@@ -1,24 +1,28 @@
 import createPalette from 'material-ui/styles/palette';
-import { createMuiTheme, MuiThemeProvider, } from 'material-ui/styles';
+import { createMuiTheme, MuiThemeProvider } from 'material-ui/styles';
 import grey from 'material-ui/colors/grey';
 import pink from 'material-ui/colors/pink';
 import teal from 'material-ui/colors/teal';
-import { create, } from 'jss';
-import { createStyleManager, } from 'jss-theme-reactor/styleManager';
+import { create } from 'jss';
+import { createStyleManager } from 'jss-theme-reactor/styleManager';
 import jssPreset from 'jss-preset-default';
 import jssGlobal from 'jss-global';
-import { syntaxStyles, } from './syntax';
+import { syntaxStyles } from './syntax';
 
-const palette = createPalette({ primary: grey, accent: pink, type: 'dark', fuchsia: '#f0f', });
+const palette = createPalette({
+  primary: grey,
+  accent: pink,
+  type: 'dark',
+  fuchsia: '#f0f',
+});
 
-const myTheme = createMuiTheme({ palette, });
+const myTheme = createMuiTheme({ palette });
 const myManager = createStyleManager({
   theme: myTheme,
   jss: create(jssPreset(), jssGlobal()),
 });
 
-const { styleManager, theme, } = MuiThemeProvider.createDefaultContext(
-  { theme: myTheme, });
+const { styleManager, theme } = MuiThemeProvider.createDefaultContext({ theme: myTheme });
 
 styleManager.render(syntaxStyles);
-export { styleManager, theme, };
+export { styleManager, theme };
