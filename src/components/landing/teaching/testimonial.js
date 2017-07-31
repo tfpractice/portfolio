5d8c94a6c8a4df4af4cf63c37c72566debfdda15
theory friction practice <jackson.brebnor@gmail.com>
2017-07-31 15:19:47 -0400

    adds chiara's review

--- src/components/landing/teaching/testimonial.js
+++ src/components/landing/teaching/testimonial.js
@@ -2,23 +2,35 @@ import React from 'react';
 import Grid from 'material-ui/Grid';
 import Text from 'material-ui/Typography';
 import Avatar from 'material-ui/Avatar';
-import Card, { CardActions, CardContent, CardMedia, CardHeader, } from 'material-ui/Card';
-import { Expand, HexCard, } from '../../misc';
+import Card, {
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+} from 'material-ui/Card';
+import { Expand, HexCard } from '../../misc';
 
 const imgUrl = 'http://via.placeholder.com/350/00ff00/ffffff?text=_';
-const Testimonial = ({ student, }) => (
-  <Grid container align="center" justify="center">
+const Testimonial = ({ student }) =>
+  (<Grid container align="center" justify="center">
     <Grid item xs>
       <HexCard raised>
         <CardHeader
-          avatar={ <Avatar src={student.imgUrl}/>}
+          avatar={<Avatar src={student.imgUrl} />}
           title={student.name}
-          subheader={student.position} />
+          subheader={student.position}
+        />
         <Grid container align="center" justify="center">
           <Grid item xs={11}>
             <CardContent>
-              <Expand open={false}
-                header={<Text color="inherit" type="title" >{student.caption}</Text>}>
+              <Expand
+                open={false}
+                header={
+                  <Text type="subheading">
+                    {student.caption}
+                  </Text>
+                }
+              >
                 <Text color="secondary" type="body1">
                   {student.content}
                 </Text>
@@ -28,7 +40,6 @@ const Testimonial = ({ student, }) => (
         </Grid>
       </HexCard>
     </Grid>
-  </Grid>
-);
+  </Grid>);
 
-export default (Testimonial);
+export default Testimonial;
--- src/components/landing/teaching/view.js
+++ src/components/landing/teaching/view.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
 import Text from 'material-ui/Typography';
-import Card, { CardActions, CardContent, CardHeader, } from 'material-ui/Card';
-import { MarkdownPreview, } from 'react-marked-markdown';
-import { Expand, HexCard, } from '../../misc';
-import { content, Wesley, } from './content';
+import Card, { CardActions, CardContent, CardHeader } from 'material-ui/Card';
+import { MarkdownPreview } from 'react-marked-markdown';
+import { Expand, HexCard } from '../../misc';
+import { Chiara, content, Wesley } from './content';
 import Testimonial from './testimonial';
 
-const Teaching = () => (
-  <Grid container justify="center" align="center">
+const Teaching = () =>
+  (<Grid container justify="center" align="center">
     <Grid item xs={11}>
       <HexCard>
         <CardHeader title="Autodidact and Educator" />
         <CardContent>
           <Text component="div" color="secondary" type="body2">
-            <MarkdownPreview value={content}/>
+            <MarkdownPreview value={content} />
           </Text>
         </CardContent>
       </HexCard>
     </Grid>
     <Grid item xs={11}>
-      <Expand header={
-        <Text color="inherit" type="display1" children="Reviews" />}>
-        <Grid container justify="center" align="center">
-          {[ ...Array(4).keys(), ].map((t, i) => (
-            <Grid item xs={11} sm={6} key={i}>
-              <Testimonial student={Wesley} key={i}/>
-            </Grid>
-          ))}
+      <Expand
+        header={<Text color="inherit" type="display1" children="Reviews" />}
+      >
+        <Grid container justify="center">
+          {[ Wesley, Chiara ].map((t, i) =>
+            (<Grid item xs={11} sm={6} key={i}>
+              <Testimonial student={t} key={i} />
+            </Grid>)
+          )}
         </Grid>
       </Expand>
     </Grid>
-  </Grid>
-);
+  </Grid>);
 
-export default (Teaching);
+export default Teaching;
