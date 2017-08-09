177ec7ce80d780824ce6d75a807da20528877b68
theory friction practice <jackson.brebnor@gmail.com>
2017-08-09 17:32:04 -0400

    captions

--- src/components/projects/single/pages/filmRatr/content.js
+++ src/components/projects/single/pages/filmRatr/content.js
@@ -1,16 +1,16 @@
 const sync = {
   caption: 'synchronizes remote and local databases',
-  content: ``,
+  content: `### synchronizes remote and local databases`,
 };
 
 const routes = {
   caption: 'performs route based database aggregations',
-  content: ``,
+  content: `### performs route based database aggregations`,
 };
 
 const serve = {
   caption: 'renders entirely from the server upon initial load',
-  content: ``,
+  content: `### renders entirely from the server upon initial load`,
 };
 
 export const slides = [ sync, routes, serve ];
