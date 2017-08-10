403c570faf929fa4b380b2c0901060f7f7623290
theory friction practice <jackson.brebnor@gmail.com>
2017-08-10 17:44:42 -0400

    curry slides

--- src/components/projects/single/pages/graph/slides.js
+++ src/components/projects/single/pages/graph/slides.js
@@ -1,95 +1,107 @@
-export const slides = [
-  {
-    caption: `functionally implements classical graph algortihms `,
+const keyless = {
+  caption: `enables keyless, node-agnostic graphs`,
+  content: `### enables keyless, node-agnostic graphs
 
-    content: `### functionally implements classical graph algortihms
+~~~js
+// creates six node objects
+const sixNodes = [ ...Array(6).keys() ].map(x => 'n'+x);
+sixNodes .... [ 'n0', 'n1', 'n2', 'n3', 'n4', 'n5' ]
+
+// adds them all to a graph
+const sixGraph = graph(...sixNodes);
+sixGraph .... Map {
+     'n0' => Map {},
+     'n1' => Map {},
+     'n2' => Map {},
+     'n3' => Map {},
+     'n4' => Map {},
+     'n5' => Map {} }
+
+// only retrieves the even nodes
+const evenNodes = sixNodes.filter((n, i) => i % 2 === 0);
+evenNodes ..... [ 'n0', 'n2', 'n4' ]
+
+// add edges between all even nodes
+const evenGraph = evenNodes.reduce(
+  (gr, nd) => addEdges(gr)(nd)(...evenNodes),
+  sixGraph
+);
+
+evenGraph ..... Map {
+      'n0' => Map { 'n0' => 0, 'n2' => 0, 'n4' => 0 },
+      'n1' => Map {},
+      'n2' => Map { 'n0' => 0, 'n2' => 0, 'n4' => 0 },
+      'n3' => Map {},
+      'n4' => Map { 'n0' => 0, 'n2' => 0, 'n4' => 0 },
+      'n5' => Map {} }
+~~~
+`,
+};
+const algortihms = {
+  caption: `functionally implements classical graph algortihms `,
+
+  content: `### functionally implements classical graph algortihms
   ~~~javascript
-  // **isIterable** :: obj -> bool   
-  // checks if an object is iterable
-  isIterable({name:john, age:12})// false
-  isIterable([1,2,3])// true
-  isIterable(new Set([1,2,3])) // true
-
-  // **iterify** :: obj -> iterable   
-  // returns the object or an Iterable<a> containging the object
-  export const iterify = o => isIterable(o) ? o : [ o, ];
+  const depthn2 = dfs(evenGraph)(evenNodes[1]);
+  const breadtn2 = bfs(evenGraph)(evenNodes[1]);
+      
+  // depth first search from start to all connected nodes
+  depthn2 Map {
+    'n2' => { pred: 'n2', length: 2, weight: 0 },
+    'n0' => { pred: 'n0', length: 4, weight: 0 },
+    'n4' => { pred: 'n4', length: 8, weight: 0 } }
   
-  // **spread** :: Iterable<a> -> Iterable<a> 
-  // returns an Iterable<a> of the collections default iterator
-  export const spread = (coll = []) => [ ...iterify(coll), ];
   
+  // breadth first search from start to all connected nodes
+  breadtn2 Map {
+    'n2' => { pred: null, length: 1, weight: 0 },
+    'n0' => { pred: 'n2', length: 2, weight: 0 },
+    'n4' => { pred: 'n0', length: 3, weight: 0 } }
+    
+    
+  // maps each node to a set of connected nodes
+  components(evenGraph) Map {
+    'n0' => Set { 'n0', 'n2', 'n4' },
+    'n2' => Set { 'n0', 'n2', 'n4' },
+    'n4' => Set { 'n0', 'n2', 'n4' },
+    'n1' => Set { 'n1' },
+    'n3' => Set { 'n3' },
+    'n5' => Set { 'n5' } }
+  
+  // returns maximal set of all connected components
+  componentSet(evenGraph) Set {
+    Set { 'n0', 'n2', 'n4' },
+    Set { 'n1' },
+    Set { 'n3' },
+    Set { 'n5' } }
   ~~~
 `,
-  },
-  {
-    caption: `retains an error-proof API via immutability and partial application`,
-    content: `### retains an error-proof API via immutability and partial application
-  
-  ~~~js
-// **first** :: Iterable<a> -> a ~ 
-// returns the first element of an iterable
-export const first = (c = []) => spread(c).shift();
+};
+const collected = {
+  caption: `leverages es6 collections to avoid custom-classes and ease extensions`,
+  content: `### leverages es6 collections to avoid custom-classes and ease extensions
 
-// **last** :: Iterable<a> -> a~  
-// returns the last element of an iterable
-export const last = (c = []) => spread(c).pop();
-  
-// **map** :: Iterable<a>  -> (a->b) -> [b]
-// returns an Iterable<a> of the return values of a
-// function called on each element of an iterable
-export const map = coll => fn => spread(coll).map(fn);
+~~~js
+// here are the graph algortihms extended in my game grid libary
+import { Graph, } from 'graph-curry';
+import { byAdj, colAdj, negAdj, posAdj, rowAdj, } from './filter';
 
-// **mapTo** :: (a->b) -> Iterable<a>  -> [b]
-// returns an Iterable<a> of the return values of a
-// function called on each element of an iterable
-export const mapTo = fn => coll => map(coll)(fn);
+const { addEdges, nodes, } = Graph;
+
+// **joinAdj** '::  (Map<edge>, node)  -> Map<edge>'
+// returns a copy of a grid with edges joining a nodes and all its neighbors
+export const joinAdj = (g, n) => addEdges(g)(n, 0)(...byAdj(nodes(g))(n));
+
+// **joinCols** '::  (Map<edge>, node)  -> Map<edge>'
+// returns a copy of a grid with edges joining a nodes and all its column neighbors
+export const joinCols = (g, n) => addEdges(g)(n, 0)(...colAdj(nodes(g))(n));
+
+// **joinRows** '::  (Map<edge>, node)  -> Map<edge>'
+// returns a copy of a grid with edges joining a nodes and all its row neighbors
+export const joinRows = (g, n) => addEdges(g)(n, 0)(...rowAdj(nodes(g))(n));
+
 ~~~
 `,
-  },
-  {
-    caption: `enables keyless, node-agnostic graphs`,
-    content: `### enables keyless, node-agnostic graphs
-  
-  ~~~js
-  // **inter** :: Iterable<a> -> Iterable<a> -> [a ]  
-  // returns elements shared between two iterables;
-  export const inter = c0 => c1 => spread(c0).filter(hasK(c1));
+};
 
-// **diff** :: Iterable<a> -> Iterable<a> -> [a ]  
-// returns elements of the first iterable absent from the second iterable
-export const diff = c0 => c1 => spread(c0).filter(xhasK(c1));
-
-// **union** :: Iterable<a> -> Iterable<a> -> [a ]  
-// returns elements of both iterables
-export const union = c0 => c1 => spread(c0).concat(diff(c1)(c0));
-~~~
-`,
-  },
-];
-
-export const slides2 = [
-  {
-    data: `functionally implements classical graph 
-  data structures`,
-  },
-  {
-    data: `makes pure uses of JS collections, 
-  for an agnostic API`,
-  },
-  {
-    data: `implements basic serach and traversals as well as cuts,
-components, and shortest paths`,
-  },
-];
-
-//
-//
-// {
-//       "caption": "functionally implements classical graph algortihms"
-//     },
-//     {
-//       "caption": "enables keyless, node-agnostic graphs"
-//     },
-//     {
-//       "caption": "retains an error-proof API via immutability and partial application"
-//     }
+export const slides = [ keyless, algortihms, collected ];
