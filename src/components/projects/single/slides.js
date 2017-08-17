27c5b24abcd536933d05df5339dae1256bd2fd70
theory friction practice <jackson.brebnor@gmail.com>
2017-08-17 17:24:30 -0400

    slideBool

--- src/components/projects/single/slides.js
+++ src/components/projects/single/slides.js
@@ -6,20 +6,18 @@ import { connect } from 'react-redux';
 import { dStyles, pColors, slug } from '../../../utils';
 import { Expand, SwipeTabs } from '../../misc';
 import Slide from './slide';
-import { getSlides, hasSlides } from './pages';
-
-const mergeMarkdown = sArr => (slide, i) => ({ ...sArr[i], ...slide });
+import { getSlides } from './pages';
 
 const mapState = (state, { project }) => ({ slides: getSlides(slug(project)) });
 
 const JustSlides = ({ project, slides }) =>
-  slides &&
+  slides.length &&
   <SwipeTabs iHue={pColors[project.category]}>
     {slides.map((h, i) => <Slide tabLabel="." key={i} slide={h} />)}
   </SwipeTabs>;
 
 const Slides = ({ data, project, slides, ...props }) =>
-  slides &&
+  slides.length &&
   <Grid container justify="center" align="center" spacing={0}>
     <Grid item xs>
       <Expand
