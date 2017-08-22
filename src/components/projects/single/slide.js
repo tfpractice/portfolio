9bff1f4d3898d57f2889e224eac2bda75764e7cb
theory friction practice <jackson.brebnor@gmail.com>
2017-08-22 16:33:18 -0400

    styles slides"

--- src/components/projects/single/slide.js
+++ src/components/projects/single/slide.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import Text from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+import Card, { CardMedia, CardContent } from 'material-ui/Card';
+import List, { ListItem, ListItemText } from 'material-ui/List';
 import { MarkdownPreview } from 'react-marked-markdown';
 
 import { dStyles, pColors, slug } from '../../../utils';
 
 const mOpts = {
-  sanitize: false,
-  smartypants: true,
+  markedOptions: {
+    sanitize: false,
+    smartypants: true,
+  },
 };
 
 const style = { backgroundColor: `rgba(40, 44, 52, 0.5)` };
 
 const Slide = ({ slide, project }) =>
-  (<Text className="slideText" type="body1" style={style} align="center">
-    <MarkdownPreview value={slide.content} markedOptions={mOpts} />
+  (<Text className="slide" align="center" style={style} component={CardMedia}>
+    <MarkdownPreview value={slide.content} {...mOpts} />
   </Text>);
 
 export default Slide;
